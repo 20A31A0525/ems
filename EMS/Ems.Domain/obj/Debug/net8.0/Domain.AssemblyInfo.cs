0f37c3d7f9a32a5fdb73ad3cc60eacb548b727bc
20A31A0525 <vanapallikusuma222@gmail.com>
2025-08-30 15:23:13 +0530

    rebuilded

--- EMS/Ems.Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ EMS/Ems.Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7328bf22a2a853539e439c82c10f1543fae7364")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
